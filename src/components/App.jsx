7dffe413df1bc65ed23fd512f1f20943e144a171
Grygorii Gontarchuk <105290753+gregorygont@users.noreply.github.com>
2023-07-23 11:09:11 +0300

    common styled components done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,33 @@
+import user from './Profile/user.json';
+import { Profile } from './Profile/Profile';
+
+
+import { GlobalStyle } from '../GlobalStyle';
+import { Box } from './Box';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="space-between"
+      as="main"
+      px={5}
+      py={3}
+      width={1}
+      bg="muted">
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      {/* <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+       <GlobalStyle /> */}
+    </Box>
   );
 };
