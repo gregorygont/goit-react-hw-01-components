7f10b8fb9fc919d06acaeb97d163b0cd165301bf
Grygorii Gontarchuk <105290753+gregorygont@users.noreply.github.com>
2023-07-24 00:48:46 +0300

    final version

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,14 @@
 import user from './Profile/user.json';
 import { Profile } from './Profile/Profile';
 
+import data from './Statistics/data.json';
+import { Statistics } from './Statistics/Statistics';
+
+import friends from './Friends/friends.json';
+import { FriendList } from './Friends/FriendList/FriendList';
+
+import transactions from './TransactionHistory/transactions.json';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 
 import { GlobalStyle } from '../GlobalStyle';
 import { Box } from './Box';
@@ -24,10 +32,10 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-      {/* <Statistics title="Upload stats" stats={data} />
+      <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
       <TransactionHistory items={transactions} />
-       <GlobalStyle /> */}
+       <GlobalStyle />
     </Box>
   );
 };
